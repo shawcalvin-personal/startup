9246f255cf3e8943ba51a8d084dc2a36580f1928
shawcalvin <calvinshaw56@gmail.com>
2023-11-09 15:23:20 -0700

    Configured startup to listen on correct port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
