af4c438b552ac27fa3042ee0e28201743b50ea6d
shawcalvin <calvinshaw56@gmail.com>
2023-12-05 17:03:23 -0700

    Updated footer text color

--- src/app.jsx
+++ src/app.jsx
@@ -65,8 +65,8 @@ function App() {
 
         <footer className='bg-dark text-dark text-muted'>
           <div className='container-fluid'>
-            <span className='text-reset'>Author Name(s)</span>
-            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+            <span className='footer-text'>Calvin Shaw</span>
+            <a className='footer-text' href='https://github.com/webprogramming260/simon-react'>
               Source
             </a>
           </div>
